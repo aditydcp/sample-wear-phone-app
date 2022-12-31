896d7d94dc69bfbcfdcfdc7851edf5a7d9b59c12
Aditya Cahyo <aditydcp@gmail.com>
2023-01-01 02:39:31 +0700

    handle message listening

--- wear/src/main/java/com/example/samplewearmobileapp/MainActivity.kt
+++ wear/src/main/java/com/example/samplewearmobileapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.samplewearmobileapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.example.samplewearmobileapp.databinding.ActivityMainBinding
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), GoogleApiClient.ConnectionCallbacks {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var client: GoogleApiClient
+    private var currentMessage: Message? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,34 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.root.findViewById<TextView>(R.id.text).text = getString(R.string.default_message)
+        binding.root.findViewById<TextView>(R.id.message).text = getString(R.string.message_placeholder)
+
+        // build Google API Client with access to Wearable API
+        client = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addApi(Wearable.API)
+            .build()
+        client.connect()
+
     }
+
+    override fun onConnected(bundle: Bundle?) {
+        Wearable.MessageApi.addListener(client) { messageEvent ->
+            currentMessage = Gson().fromJson(String(messageEvent.data), Message::class.java)
+            updateView()
+        }
+    }
+
+    override fun onConnectionSuspended(code: Int) {
+        Log.w("Wear", "Google Api Client connection suspended!")
+    }
+
+    private fun updateView() {
+        currentMessage?.let {
+            binding.root.findViewById<TextView>(R.id.text).text = getString(R.string.updated_message)
+            binding.root.findViewById<TextView>(R.id.message).text = it.content
+        }
+    }
+
 }
