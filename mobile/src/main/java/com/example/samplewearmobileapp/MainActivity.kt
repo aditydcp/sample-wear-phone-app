12d9e46c78f7bafd9c9ed15e383854e7c303a9cf
Aditya Cahyo <aditydcp@gmail.com>
2023-01-01 02:39:07 +0700

    handle message sending

--- mobile/src/main/java/com/example/samplewearmobileapp/MainActivity.kt
+++ mobile/src/main/java/com/example/samplewearmobileapp/MainActivity.kt
@@ -1,24 +1,55 @@
 package com.example.samplewearmobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.samplewearmobileapp.databinding.ActivityMainBinding
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var client: GoogleApiClient
+    private var connectedNode: List<Node>? = null
+    private lateinit var message: Message
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // build Google API Client with access to Wearable API
+        client = GoogleApiClient.Builder(this)
+            .addApi(Wearable.API)
+            .addConnectionCallbacks(this)
+            .build()
+        client.connect()
+
+        // create a message object
+        message.content = getString(R.string.message_content)
+
         binding.root.findViewById<Button>(R.id.button).setOnClickListener {
-            onButtonClicked()
+            onButtonClicked(message)
         }
     }
 
-    fun onButtonClicked() {
-        TODO("Handle button click by sending message to the Wear module")
+    private fun onButtonClicked(message: Message) {
+        val gson = Gson()
+        connectedNode?.forEach { node ->
+            val bytes = gson.toJson(message).toByteArray()
+            Wearable.MessageApi.sendMessage(client, node.id, "/message", bytes)
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        Wearable.NodeApi.getConnectedNodes(client).setResultCallback {
+            connectedNode = it.nodes
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        connectedNode = null
     }
 }
