dfdba6f075019bf87184a44bae7a80f88cda67b1
Aditya Cahyo <aditydcp@gmail.com>
2023-05-12 01:29:30 +0700

    change all data to send every 12 secs

--- wear/src/main/java/com/example/samplewearmobileapp/Constants.kt
+++ wear/src/main/java/com/example/samplewearmobileapp/Constants.kt
@@ -7,6 +7,12 @@ object Constants {
     const val MS_TO_SEC = .001
 
     /**
+     * Time covered in 1 data batch in ms.
+     */
+    const val BATCHING_DURATION: Long = 12000
+    // 12000 ms = 12 sec
+
+    /**
      * Sampling rate for PPG Green.
      * @see PPG_IR_RED_SAMPLE_RATE
      */
@@ -32,6 +38,13 @@ object Constants {
     // each second 100 samplings happen
 
     /**
+     * The rate used for sending PPG IR and Red sample batch in ms.
+     */
+    const val PPG_IR_RED_CUSTOM_TICK_RATE: Long = PPG_GREEN_TICK_RATE
+    // the same as PPG_GREEN_TICK_RATE = 12000 ms = 12 sec.
+    // this is implemented due to flooding when data are sent each second
+
+    /**
      * The number of PPG Green data points
      * to be included in 1 batch.
      */
@@ -39,13 +52,26 @@ object Constants {
         (PPG_GREEN_SAMPLE_RATE * PPG_GREEN_TICK_RATE * MS_TO_SEC).toInt()
     // 25 Hz * 12 sec = 300 data points
 
+//    /**
+//     * The number of PPG IR and Red data points
+//     * to be included in 1 batch.
+//     */
+//    const val PPG_IR_RED_BATCH_SIZE: Int =
+//        (1 * PPG_IR_RED_SAMPLE_RATE).toInt() // = 100 data points
+//    // using tick rate would be too small
+//    // and causing data loss in transportation
+//    // so instead, batch data every 1 second(s)
+
     /**
      * The number of PPG IR and Red data points
      * to be included in 1 batch.
      */
     const val PPG_IR_RED_BATCH_SIZE: Int =
-        (1 * PPG_IR_RED_SAMPLE_RATE).toInt() // = 100 data points
-    // using tick rate would be too small
-    // and causing data loss in transportation
-    // so instead, batch data every 1 second(s)
+        (PPG_IR_RED_SAMPLE_RATE * PPG_IR_RED_CUSTOM_TICK_RATE * MS_TO_SEC).toInt()
+    // 100 Hz * 12 sec = 12000 data points
+    // using actual tick rate OR 1 second would mean sending too frequently
+    // causing the mobile module to hang and flooding the data queue
+    // so instead, safely use 12 sec (also to match the PPG Green batch)
+
+
 }
